0f1194a80cdd417ae687589b416b10ed0b5d4c3e
Svetlana <Sharunkina2014@yandex.ru>
2024-10-22 12:31:56 +0300

    commit

---
+++ src/Addition.java
@@ -0,0 +1,8 @@
+public class Addition implements ComplexOperation {
+    @Override
+    public ComplexNumber execute(ComplexNumber a, ComplexNumber b) {
+        double real = a.getReal() + b.getReal();
+        double imaginary = a.getImaginary() + b.getImaginary();
+        return new ComplexNumber(real, imaginary);
+    }
+}
